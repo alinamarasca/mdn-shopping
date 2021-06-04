7ddbe207759cd00b664048c5492041310b8dbdc3
HYF <alinamarasca@gmail.com>
2021-06-04 14:53:40 +0200

    change div name

--- src/init/index.js
+++ src/init/index.js
@@ -6,21 +6,21 @@ console.log(bigDiv);
 
 // div for MAKING LIST
 const createList = document.createElement('DIV');
-inputDiv.setAttribute('id', 'create-list');
+createList.setAttribute('id', 'create-list');
 // console.log(inputDiv);
-bigDiv.appendChild(inputDiv);
+bigDiv.appendChild(createList);
 
 // INPUT FIELD
 const inputField = document.createElement('input');
 inputField.setAttribute('type', "text");
 // console.log(inputField);
-inputDiv.appendChild(inputField);
+createList.appendChild(inputField);
 
 // ADD btn
 const addBtn = document.createElement('button');
 addBtn.setAttribute('id', 'add-btn');
 addBtn.setAttribute('class', 'btn');  
 addBtn.textContent = 'add';
-inputDiv.appendChild(addBtn);
+createList.appendChild(addBtn);
 
 // create div to see the list
