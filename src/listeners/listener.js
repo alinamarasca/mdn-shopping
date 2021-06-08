b76ad8ab102e1703180065697e52704689f31001
HYF <alinamarasca@gmail.com>
2021-06-08 10:28:41 +0200

    correct

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -3,5 +3,5 @@ import {addBtn} from "../init/index.js"
 // // const inputField = document.createElement('input');
 // // inputField.addEventListener('mouseover', (e) => console.log('you type'));
 // const addBtn = document.createElement('button');
-getElementById('add-btn').addEventListener('click', (e) => console.log('hey'))
+addBtn.addEventListener('click', (e) => console.log('hey'))
 
