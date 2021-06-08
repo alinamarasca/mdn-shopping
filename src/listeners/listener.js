a21230d55a3ec88e41a49c48184e9e142a361813
HYF <alinamarasca@gmail.com>
2021-06-08 15:33:13 +0200

    reorganize, css

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -1,7 +1,6 @@
-import {addBtn} from "../init/index.js"
+import { addBtn } from "../init/ui.js"
+import { dummyHandler, dummyHandler1 } from '../handlers/handlers.js'
 
-// // const inputField = document.createElement('input');
-// // inputField.addEventListener('mouseover', (e) => console.log('you type'));
-// const addBtn = document.createElement('button');
-addBtn.addEventListener('click', (e) => console.log('hey'))
 
+
+addBtn.addEventListener('click', dummyHandler)
