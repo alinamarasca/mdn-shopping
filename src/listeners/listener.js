2f8ebb10597658c1f8a17d5f8846a4e3bff17b33
HYF <alinamarasca@gmail.com>
2021-06-08 10:27:16 +0200

    last attempts

---
+++ src/listeners/listener.js
@@ -0,0 +1,7 @@
+import {addBtn} from "../init/index.js"
+
+// // const inputField = document.createElement('input');
+// // inputField.addEventListener('mouseover', (e) => console.log('you type'));
+// const addBtn = document.createElement('button');
+getElementById('add-btn').addEventListener('click', (e) => console.log('hey'))
+
