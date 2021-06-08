ea96261c76048d9f2e3c815b8fb6f75cef67bf81
HYF <alinamarasca@gmail.com>
2021-06-08 18:34:59 +0200

    upd

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -7,12 +7,19 @@ export const addItem = (event) => {
    const itemDiv = document.createElement('div');
    itemDiv.classList.add('item-div');
    theList.appendChild(itemDiv);
+
+
    //li
    const listItem = document.createElement('li');
    listItem.innerText = inputField.value;
    listItem.classList.add('item');
-   itemDiv.appendChild(listItem);
+      itemDiv.appendChild(listItem);
    inputField.value = '';
+   //delete
+   const deleteBtn = document.createElement('button');
+   deleteBtn.innerHTML = '<i class = "fas fa-trash"></i>';
+   deleteBtn.classList.add('delete-btn');
+   itemDiv.appendChild(deleteBtn);
 }
 
 
